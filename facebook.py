d47b2cbe24914fb14d5c2f0571f9352ac43f31e5
mai21-meet <52973936+mai21-meet@users.noreply.github.com>
2019-11-24 19:43:21 +0200

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -5,12 +5,12 @@ class Post():
 		self.text=text
 
 class User():
-	def __init__ (self,name,email,password,friends_list=[],posts=[]):
+	def __init__ (self,name,email,password):
 		self.name=name
 		self.email=email
 		self.password=password
 		self.friends_list = []
-		self.posts = []
+		self.post = []
 
 	def add_friends(self):
 		email = input("Enter your friend's email")
@@ -25,7 +25,7 @@ class User():
 	def posts(self):
 		text =input("give title to your post")
 		post1=Post(self.name, "17/11", text)
-		self.posts.append(post1)
+		self.post.append(post1)
 		print(self.name+"has posted"+text)
 
 	def get_userinfo(self):
@@ -33,7 +33,16 @@ class User():
 		print("Email: " + self.email)
 		print("passwors: " +self.password)
 		print("friends list: " + str(self.friends_list))
-		print("posts: " + str(self.posts))
+		print("posts: " + str(self.post))
+
+class Comment(Post):
+	def __init__ (self,text,likes,date):
+		self.likes=likes
+		self.date=date
+
+	def writecomment(self):
+		print(input("write your comment"))
+		print(self.text)
 
 user1 = User('Mai',"mai@gmail.com","omer")
 user1.add_friends()
@@ -44,4 +53,6 @@ user2=User('omer','omer@gmail.com','mai')
 user2.add_friends()
 user2.posts()
 user2.remove_friend()
+
+#my stomek hets so lets practis my writing on the computer. i am bored,we have 33 minutes to the end of the saaion. i think tha evry one thinks im working,when ectully i am writing this.
 
